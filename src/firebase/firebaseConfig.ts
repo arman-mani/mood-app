d8beacca9bab56e5341072bf9068a2900b672902
unknown <arman.karimi@chasacademy.se>
2025-05-07 02:49:48 +0200

    auth

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -2,15 +2,14 @@
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
-
 // The web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB005ODvguiru8ztSUyUFp93OV7E1VCqjs",
-  authDomain: "moody-ff022.firebaseapp.com",
-  projectId: "moody-ff022",
-  storageBucket: "moody-ff022.appspot.com",
-  messagingSenderId: "149301168861",
-  appId: "1:149301168861:web:56d5c777adb078984fa934"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyB005ODvguiru8ztSUyUFp93OV7E1VCqjs",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "moody-ff022.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "moody-ff022",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "moody-ff022.appspot.com",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "149301168861",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:149301168861:web:56d5c777adb078984fa934"
 };
 
 // Initialize Firebase
